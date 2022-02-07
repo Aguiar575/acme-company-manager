23b9aa27d8ce2819bddba766535a62305051babc
Aguiar575 <aguiarus99arthurus@gmail.com>
2022-02-07 01:47:34 -0300

    InsertSpaces extended in Product;

--- ACM/ACM.BL/Product.cs
+++ ACM/ACM.BL/Product.cs
@@ -1,29 +1,42 @@
-﻿namespace ACM.BL
+﻿using ACM.Common;
+
+namespace ACM.BL
 {
-  public class Product : EntityBase
+    public class Product : EntityBase
     {
-    public Product()
-    {
+        public Product()
+        {
 
+        }
+        public Product(int productId)
+        {
+            ProductId = productId;
+        }
+
+        public decimal? CurrentPrice { get; set; }
+        public string ProductDescription { get; set; }
+        public int ProductId { get; private set; }
+        private string _productName;
+        public string ProductName
+        {
+            get
+            {
+                return _productName.InsertSpaces();
+            }
+            set
+            {
+                _productName = value;
+            }
+        }
+
+        public override bool Validate()
+        {
+            var isValid = true;
+
+            if (string.IsNullOrWhiteSpace(ProductName)) isValid = false;
+            if (CurrentPrice == null) isValid = false;
+
+            return isValid;
+        }
     }
-    public Product(int productId)
-    {
-      ProductId = productId;
-    }
-
-    public decimal? CurrentPrice { get; set; }
-    public string ProductDescription { get; set; }
-    public int ProductId { get; private set; }
-    public string ProductName { get; set; }
-
-    public override bool Validate()
-    {
-      var isValid = true;
-
-      if (string.IsNullOrWhiteSpace(ProductName)) isValid = false;
-      if (CurrentPrice == null) isValid = false;
-
-      return isValid;
-    }
-  }
 }
